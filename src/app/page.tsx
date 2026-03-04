bbe5b417007c62e2572dab32e4feb7672576314a
Mahmoud-abu-zaid <v.i.p.omlat@gmail.com>
2026-03-04 13:34:11 +0200

    feat(hero): add organized responsive hero videos by screen size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import HeroSectionVideo from "@/components/pages/hero-section";
+
 export default function Home() {
-  return <div className="">
-    <h1>Home Page</h1>
-  </div>;
+  return (
+    <div >
+      <HeroSectionVideo />
+    </div>
+  );
 }
