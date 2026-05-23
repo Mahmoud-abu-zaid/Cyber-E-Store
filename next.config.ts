2460ce611af772e2228001ae0530dfd6880cac4e
Mahmoud-abu-zaid <mahmoudabuzaid122@gmail.com>
2026-05-23 20:06:55 +0300

    feat(config): add supabase and dummyjson to image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "zweodqtulvtcdwdnfjvy.supabase.co",
+      },
       {
         protocol: "https",
         hostname: "cdn.dummyjson.com",
