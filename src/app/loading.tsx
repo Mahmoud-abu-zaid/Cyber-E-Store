c12f511e53f9727fee5e464fca78498ca2074b19
Mahmoud-abu-zaid <mahmoudabuzaid122@gmail.com>
2026-05-23 20:10:32 +0300

    feat(ux): add welcome message with first and last name in loading screen

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,13 @@
+"use client";
 import { motion, AnimatePresence } from "framer-motion";
 import Image from "next/image";
+import { useProfile } from "@/modules/account/hooks/use-profile";
 
 type LoadingProps = {
   isLoading: boolean;
 };
-
 export default function Loading({ isLoading }: LoadingProps) {
+  const { data: profile } = useProfile();
   return (
     <AnimatePresence>
       {isLoading && (
@@ -14,12 +16,16 @@ export default function Loading({ isLoading }: LoadingProps) {
           initial={{ opacity: 1 }}
           animate={{ opacity: 0 }}
           transition={{ delay: 2, duration: 0.6 }}
-          onAnimationComplete={isLoading ? undefined : () => {}}
+          onAnimationComplete={isLoading ? undefined : () => { }}
         >
           <div className="relative w-85 h-40 overflow-hidden">
             <motion.div className="absolute top-0 left-0 w-full h-1/2 bg-white z-10" initial={{ y: 0 }} animate={{ y: "-100%" }} transition={{ duration: 1.2, ease: "easeInOut" }} />
             <motion.div className="absolute bottom-0 left-0 w-full h-1/2 bg-white z-10" initial={{ y: 0 }} animate={{ y: "100%" }} transition={{ duration: 1.2, ease: "easeInOut" }} />
             <Image src="/img/Website logo.webp" alt="Logo" width={420} height={120} priority />
+
+            <div className="flex items-center justify-center pt-2"> {profile?.first_name ? (
+              <h2 className="text-center text-2xl">Welcome {profile.first_name}  {profile.last_name}!</h2>) : ""}
+            </div>
           </div>
         </motion.div>
       )}
