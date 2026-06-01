ee0425567033a5346349b77088918d26a27cea7f
Mahmoud-abu-zaid <mahmoudabuzaid122@gmail.com>
2026-06-01 11:12:26 +0300

    feat(about): add placeholder content with future update message

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,11 @@
+import Image from "next/image";
+
 export default function About() {
-  return <div>About Page</div>;
+  return <div className="pt-16 px-2 2xl:container 2xl:mx-auto h-[70vh] flex justify-center items-center">
+    <div className="flex flex-col justify-center items-center gap-1">
+      <Image src="/img/Website icon.webp" alt="bestseller" width={100} height={100} className="py-3" loading="lazy" />
+      <p className="text-2xl font-semibold">we{"'"}re coming soon..</p>
+      <p className="font-semibold text-lg"><em>we{"'"}re working on our new website. </em> </p>
+    </div>;
+  </div>
 }
