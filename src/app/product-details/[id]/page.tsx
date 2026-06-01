d3852fe59b416fb5a76540b81b261ad9ab22b1c5
Mahmoud-abu-zaid <mahmoudabuzaid122@gmail.com>
2026-06-01 11:04:54 +0300

    chore(seo): add dynamic metadata for product details page

--- src/app/product-details/[id]/page.tsx
+++ src/app/product-details/[id]/page.tsx
@@ -1,4 +1,30 @@
+import axios from "axios";
+import { Metadata } from "next";
+import { Product } from "@/modules/discover-products/Types/products";
 import ProductDetailsClient from "@/modules/product-details/components/product-details-client";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { id } = await params;
+  const { data: product } = await axios.get<Product>(
+    `https://dummyjson.com/products/${id}`
+  );
+
+  return {
+    title: `${product.title} `,
+    description: product.description,
+    keywords: [product.title, product.brand, product.category, "Cyber E-Store"],
+    openGraph: {
+      title: product.title,
+      description: product.description,
+      images: [{ url: product.thumbnail }],
+      type: "website",
+    },
+  };
+}
 
 export default async function ProductDetails({
   params,
