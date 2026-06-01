762a36446112cc7b135898e019259924139b6bf0
Mahmoud-abu-zaid <mahmoudabuzaid122@gmail.com>
2026-06-01 11:15:21 +0300

    chore(seo): add metadata for register page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,9 +1,15 @@
+import type { Metadata } from "next";
 import RegisterForm from "@/modules/auth/register/components/register-form";
 
+export const metadata: Metadata = {
+  title: "Create Account ",
+  description: "Create a new Cyber E-Store account to start shopping.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
 export default function Register() {
-  return (
-    <div>
-      <RegisterForm />
-    </div>
-  );
+  return <RegisterForm />
 }
