5d35e3f26f2d77a9c353a84e42c175848c6638cd
Mahmoud-abu-zaid <mahmoudabuzaid122@gmail.com>
2026-06-01 11:15:33 +0300

    chore(seo): add metadata for login page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,9 +1,15 @@
+import type { Metadata } from "next";
 import LoginForm from "@/modules/auth/login/components/login-form";
 
+export const metadata: Metadata = {
+  title: "Login ",
+  description: "Sign in to your Cyber E-Store account to access your orders, wishlist, and more.",
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
 export default function Login() {
-  return (
-    <div>
-      <LoginForm />
-    </div>
-  );
+  return <LoginForm />
 }
