c95d1e6783e4a670b3001d515b42b4dbab40cd83
Mahmoud-abu-zaid <mahmoudabuzaid122@gmail.com>
2026-06-06 21:50:35 +0300

    perf(animation): replace motion with m in AnimateOnScroll

--- src/components/animate-on-scroll.tsx
+++ src/components/animate-on-scroll.tsx
@@ -1,47 +1,35 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { m } from "framer-motion";
 
 export default function AnimateOnScroll({ children, className = "" }: { children: React.ReactNode; className?: string; }) {
 
   return (
     <div>
       <div className="sm:block hidden">
-        <motion.div
+        <m.div
           className={className}
           initial={{ opacity: 0, y: 15 }}
           whileInView={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 15 }}
-          viewport={{
-            once: false,
-            amount: 0.2,
-          }}
-          transition={{
-            duration: 0.4,
-            ease: [0.25, 0.1, 0.25, 1],
-          }}
+          viewport={{ once: false, amount: 0.2 }}
+          transition={{ duration: 0.4, ease: [0.25, 0.1, 0.25, 1] }}
         >
           {children}
-        </motion.div>
+        </m.div>
       </div>
 
       <div className="block sm:hidden">
-        <motion.div
+        <m.div
           className={className}
           initial={{ opacity: 0, y: 15 }}
           whileInView={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: 15 }}
-          viewport={{
-            once: false,
-            amount: 0.05,
-          }}
-          transition={{
-            duration: 0.4,
-            ease: [0.25, 0.1, 0.25, 1],
-          }}
+          viewport={{ once: false, amount: 0.05 }}
+          transition={{ duration: 0.4, ease: [0.25, 0.1, 0.25, 1] }}
         >
           {children}
-        </motion.div>
+        </m.div>
       </div>
     </div>
   );
